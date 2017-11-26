8ba03004288ce2359297eb108dde07229feb7981
davidslama <33981627+davidslama@users.noreply.github.com>
2017-11-26 18:39:28 +0100

    new feature added
    more story

--- hra.bat
+++ hra.bat
@@ -39,13 +39,16 @@ if errorlevel == 1 goto komin
 
 :komin
 
-echo uspesne si se dostal do sidla skrz komin a ziskal zpet sve penize
+echo Pres komin si pronikl dovnitr muzes prohledat bud sklep
+echo nebo patro na kterym se prave nachazis nebo pudu 
 echo.
-echo u - ukoncit hru
-echo z - hrat znovu
-choice /c zu
-if errorlevel == 2 goto konec
-if errorlevel == 1 goto zacatek
+echo n - soucasne patro
+echo s - sklep
+echo p - puda 
+choice /c psn
+if errorlevel == 3 goto soucasne
+if errorlevel == 2 goto sklep
+if errorlevel == 1 goto puda
 
 :vypacit
 
@@ -56,6 +59,51 @@ choice/c zu
 if errorlevel == 2 goto konec
 if errorlevel == 1 goto zkusit
 
+:sklep
+
+echo Proplizil ses do sklepa, bohuzel si ale nic nenasel
+echo n - jit zpatky nahoru a prohledat horni patro
+echo p - jit  na pudu a prohledat ji
+choice /c pn
+if errorlevel == 2 goto soucasne
+if errorlevel == 1 goto puda
+
+:puda
+ 
+echo plizil ses na pudu ale zistil si, ze je zamcena
+echo n - jit zpet dolu a dolni patro
+echo s - jit prohledat sklep
+choice /c sn
+if errorlevel == 2 goto soucasne
+if errorlevel == 1 goto sklep
+
+:soucasne
+
+echo Prohledal si cele patro a se stestim si nikoho z banditu nevzbudil
+echo ale nenasel si sve penize, jenom klice
+echo p - jit na pudu
+echo s - jit do sklepa
+choice /c sp
+ if errorlevel == 2 goto puda2
+ if errorlevel == 1 goto sklep2
+ 
+:puda2
+
+echo Odemkl si pudu a nasel sejf ve kterym byli tve penize
+
+echo u - ukoncit hru
+echo z - hrat znovu
+choice/c zu
+if errorlevel == 2 goto konec
+if errorlevel == 1 goto zkusit
+
+:sklep2
+
+echo Proplizil ses do sklepa, bohuzel si ale nic nenasel
+echo p - jit na pudu
+choice /c p
+if errorlevel == 1 goto puda2
+ 
 
 :zkusit
 goto zacatek
