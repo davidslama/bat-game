30b9c86dd20f220c112f250a026d0c8d283cb8ba
davidslama <33981627+davidslama@users.noreply.github.com>
2017-11-26 12:06:06 +0100

    Finished
    It now works.

--- hra.bat
+++ hra.bat
@@ -1,39 +1,64 @@
-@echo off
+echo off
 
-set /p hrac="zadej jmeno hrace:"
+
+
+set /p hrac=" Ahoj, jak se jmenujes?:"
 echo vybral sis hrace: %hrac%
-msg %username% "ahoj%hrac%e"
+msg %username% "ahoj hodne stesti "%hrac%e"
 if "%hrac%"=="" pøíkaz
+:zacatek
+echo Vystopoval si bandity, ktery te okradli, tvuj ukol je ziskat zpet sve penize.
 
-echo Tvuj ukol je ziskat lup od mistnich banditu. Kdyz dorazis na misto mas tri
-echo moznosti jak toho docilit
-
-echo a - vecer se proplizit a pokusit se lup ukrast
-echo b - probit se skrz bandity.
+echo v - pokusit se do jejich sidla proplizit vecer
+echo d - jit hlavnimi dvermi za dne
 echo.
-choice /c:ab
-rem vyberes si moznost
-
+choice /c:dv
+rem vyberes si den nebo noc.
 if errorlevel == 2 goto vecer
-if errorlevel == 1 goto probit
+if errorlevel == 1 goto den
 
+:den
+ echo sel si za dne, tim padem te bandite hned poznali a zabili.
+ echo u - ukoncit hru
+ echo z - zkusit znovu
+ echo.
+choice/c:zu
+if errorlevel == 2 goto konec
+if errorlevel == 1 goto zkusit
 
-:vecer 
-echo lup je na dosah ale jestli ho vezmes riskujes ze probudis banditi.
+:vecer
+echo Je pulnoc a ty se rozhodnes vyrazit.
+
+echo v - muzes vypacit okno a tim se dostat do sidla.
+echo k - nebo muzes vlizt do domu kominem
 echo.
-timeout /t 10
-echo c - vzit lup a utyct.
-echo g - pobit banditi ve spanku.
+choice /c kv
+rem vyberes si moznost vniknuti
+if errorlevel == 2 goto vypacit
+if errorlevel == 1 goto komin
+
+:komin
+
+echo uspesne si se dostal do sidla skrz komin a ziskal zpet sve penize
 echo.
+echo u - ukoncit hru
+echo z - hrat znovu
+choice /c zu
+if errorlevel == 2 goto konec
+if errorlevel == 1 goto zacatek
 
-choice /c:cg
+:vypacit
 
-if errorlevel == 2 goto vzit
-if errorlevel == 1 goto pobit
+echo pacenim okna si udelal moc velky hluk a bandite te zabili
+echo u - ukoncit hru
+echo z - zkusit znovu
+choice/c zu
+if errorlevel == 2 goto konec
+if errorlevel == 1 goto zkusit
 
-:vzit
 
-:pobit
+:zkusit
+goto zacatek
 
 :konec
-pause
+
